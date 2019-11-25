133623eec865c2672be76e4d730c5b02a35b46d6
Brian Fletcher <brian.fletcher@arkanosoft.com>
2019-11-25 16:51:25 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   debug: true,
+  // apiUrl: 'http://192.168.1.160:1337/',
   apiUrl: 'http://localhost:1337/',
   apiVersion: 'v1',
   appTitle: 'angular-pwa-boiler-plate Prod',
